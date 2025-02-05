ff4f984b9326c5234705ad0279a2a75df9e5edbe
ChillhzDev <jhzmoura@gmail.com>
2025-02-05 14:18:28 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,5 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("menu-container").innerHTML = data;
-        });
+        })
+        .catch(error => console.error("Erro ao carregar o menu:", error));
 });
