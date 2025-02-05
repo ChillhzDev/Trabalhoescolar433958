3c2ed92beb57b24c08f9c5c36705a63e25ae4373
ChillhzDev <jhzmoura@gmail.com>
2025-02-05 14:21:35 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("./menu/menu.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("menu-container").innerHTML = data;
-        })
-        .catch(error => console.error("Erro ao carregar o menu:", error));
-});
